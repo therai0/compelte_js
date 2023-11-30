e1429ca7995f1b2c63d1495ba36d4a4158cbf5ea
Bhaskar <raibhaskar003@gmail.com>
2023-11-30 16:30:22 +0545

    let var const

--- chaiUrCode.js
+++ chaiUrCode.js
@@ -1,1 +1,10 @@
 console.log("Hello coders")
+
+//declaring the varianle
+const userId = 123354
+let userPhoneNumber = 98123042488
+var userPassword = "thisisMyPassword"
+
+console.table([userId,userPhoneNumber,userPassword])
+
+// in the present days var keyword is not recommend for declaring variable.

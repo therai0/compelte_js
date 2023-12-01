39621530cef920cd6084bc8b3af8b992a7103027
Bhaskar <raibhaskar003@gmail.com>
2023-12-01 07:49:27 +0545

    data type

--- chaiUrCode.js
+++ chaiUrCode.js
@@ -1,10 +1,21 @@
-console.log("Hello coders")
+// console.log("Hello coders")
 
 //declaring the varianle
-const userId = 123354
-let userPhoneNumber = 98123042488
-var userPassword = "thisisMyPassword"
+// const userId = 123354
+// let userPhoneNumber = 98123042488
+// var userPassword = "thisisMyPassword"
 
-console.table([userId,userPhoneNumber,userPassword])
+// console.table([userId,userPhoneNumber,userPassword])
 
 // in the present days var keyword is not recommend for declaring variable.
+
+// "use strict"; // treat all js as a newer version.
+
+//datatype aviable in javaScript
+//  Number 2^53 limit
+// Bigint
+// String
+// null
+// boolean true/false
+// undefined
+// symbol // unique

1b5e21389b7a4ab918b850999472de10b06925a3
Bhaskar <raibhaskar003@gmail.com>
2023-12-03 16:26:49 +0545

    All method of string

---
+++ IsTring.js
@@ -0,0 +1,27 @@
+// String and its method in javaScript
+let str = "Hello"
+
+let str2 = "world"
+let newStr = str.concat(str2) // concatination the two string
+// console.log(newStr)
+
+let getChar = str.charAt(0) // it will return the  char of index 0
+// console.log(getChar);
+
+// uppperCase and  lowerCase
+// console.log(str.toLocaleLowerCase()); // it will convert the string into lower case
+// console.log(str.toLocaleUpperCase());//it will convert the string into upper case
+
+let len = str.length // it will return the length of the string
+// console.log(len)
+
+let subStr = str.substring(0, str.length-1)
+//return the substring from 0 to last index but it will not include the last index 
+// console.log(subStr);
+
+
+/*
+str.indexOf()
+str.trim()
+str.replace()
+*/

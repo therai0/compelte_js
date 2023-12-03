a2b26f7e002368bbadc42e6ea59ec21ee2dba129
Bhaskar <raibhaskar003@gmail.com>
2023-12-03 16:50:08 +0545

    All method of Array

---
+++ Array.js
@@ -0,0 +1,41 @@
+// Array are mutuable in js
+
+let arr = [12,34,5,56,6]
+
+//printing the all element of the array
+// for(let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i])
+// }
+
+let len = arr.length //it will return the length of the array
+// console.log(len)
+
+// Some operation that can be done in array
+// push() add element in last of the array
+// pop() // delement the element from the last
+//
+arr.unshift(9000) // it will add the element at the begining of the array
+// console.log(arr[0])
+arr.shift() //it will delete the element which is at the begining.
+// console.log(arr[0])
+
+let newArr = [23,45,56,67]
+let afterConcat = newArr.concat(arr) // we can even concat the two array
+// console.log(afterConcat)
+
+// splicing use to replace and delete the certain element of the array
+let a = [1,2,3,4,5]
+// console.log(a)
+// [1,2,3,4,5]
+
+a.splice(0,2,34,45)
+// element will delete from the 0 index and it will delete the 2 index of array and replace with 34 and 45 
+// of that   deleted index
+
+// console.log(a)
+// [ 34, 45, 3, 4, 5 ]
+
+// a.forEach((element)=>{
+//     console.log(element)
+// })
